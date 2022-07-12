3ef16aceeeaeecc75c51ecd0b9e5c48ff661af42
mth9 <mth9dev@gmail.com>
2022-07-12 22:09:22 +0800

    Corrected model QuestionFormGroup

--- src/app/models/question.ts
+++ src/app/models/question.ts
@@ -1,14 +1,14 @@
 import { FormArray, FormControl, FormGroup } from "@angular/forms";
 
 export interface CustomizableFormGroup {
-    questions: FormArray<FormGroup<QuestionFormGroup>>;
+    questions: FormArray<FormGroup<QuestionFormGroup>>
 }
 
 export interface QuestionFormGroup {
-    text: FormGroup<string>;
-    type: FormGroup<'paragraph' | 'checkbox'>;
-    answer: FormControl<string> | FormArray<FormGroup<{
-        option: FormControl<string>,
-        value: FormControl<boolean>
+    text: FormControl;
+    type: FormControl;
+    answer?: FormControl | FormArray<FormGroup<{
+        option: FormControl,
+        value: FormControl
     }>>;
 }
