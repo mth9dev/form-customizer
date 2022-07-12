cee3af9e85200714f8399c293fe6e2b4c04c5582
mth9 <mth9dev@gmail.com>
2022-07-12 17:22:59 +0800

    Added Builder Component
    - Created component.
    - Added route.
    - Redirected route to it in case of unkown routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LayoutComponent } from './layout/layout.component';
+import { BuilderComponent } from './pages/builder/builder.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    children: []
+    children: [
+      {
+        path: '',
+        redirectTo: 'form/builder',
+        pathMatch: 'full',
+      },
+      {
+        path: 'form/builder',
+        component: BuilderComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'form/builder',
+        pathMatch: 'full',
+      }
+    ]
   }
 ];
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { BuilderComponent } from './builder/builder.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BuilderComponent
+  ],
   imports: [
     CommonModule,
     SharedModule,
