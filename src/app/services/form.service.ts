a4cfd730c7dda66a49a034dbd31d8eff901b79e2
mth9 <mth9dev@gmail.com>
2022-07-13 01:52:04 +0800

    Manipulated question Form Group before appending
    - Append new question form group to main form using service.
    - If question type is paragraph, add required validators to answer form control.

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import { FormArray, FormGroup } from '@angular/forms';
+import { FormArray, FormGroup, Validators } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
-import { CustomizableFormGroup } from '../models/question';
+import { CustomizableFormGroup, quesionTypes } from '../models/question';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormService {
 
-  private _form = new BehaviorSubject<FormGroup<CustomizableFormGroup> | undefined>(
+  private _form = new BehaviorSubject<FormGroup<CustomizableFormGroup>>(
     new FormGroup<any>({
       'questions': new FormArray<any>([])
     })
@@ -18,7 +18,7 @@ export class FormService {
     return this._form.asObservable();
   }
 
-  set form(formGroup: FormGroup | undefined) {
+  set form(formGroup: FormGroup) {
     this._form.next(formGroup);
   }
 
@@ -26,7 +26,16 @@ export class FormService {
   ) { }
 
   addQuestion(qForm : FormGroup) {
-    console.log(qForm)
+    // make it untouched.
+    qForm.markAsUntouched();
+    // add validation
+    // if 'paragraph' type,
+    if(qForm.get('type')!.value == quesionTypes.PARAGRAPH) {
+      qForm.get('answer')?.addValidators(Validators.required);
+    }
+    let newform = (this._form.getValue());
+    (newform.get('questions') as FormArray).push(qForm);
+    this.form = newform;
   }
 
 }
