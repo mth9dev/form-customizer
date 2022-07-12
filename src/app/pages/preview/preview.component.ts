f9adb0d7c5a86389fda857559897882e3b028915
mth9 <mth9dev@gmail.com>
2022-07-13 03:56:46 +0800

    Finished Preview UI
    -Can navigate back to builder.

--- src/app/pages/preview/preview.component.ts
+++ src/app/pages/preview/preview.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Preview, questionTypes } from 'src/app/models/question';
+import { FormService } from 'src/app/services/form.service';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
   styleUrls: ['./preview.component.scss']
 })
-export class PreviewComponent implements OnInit {
+export class PreviewComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private _subs = new SubSink();
+  protected _preview: Preview | undefined;
+  protected _questionTypes = questionTypes;
+
+  constructor(
+    private _formService: FormService,
+    private _router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this._subs.sink = this._formService.preview$.subscribe(preview => {
+      this._preview = preview;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this._subs.unsubscribe();
+  }
+
+  backToBuilder() {
+    this._router.navigate(['/form/builder']);
   }
 
 }
