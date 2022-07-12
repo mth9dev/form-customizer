85a6eeb017180168421e0ab23c09d3321b177ac9
mth9 <mth9dev@gmail.com>
2022-07-13 02:12:27 +0800

    Provided "Other" option
    -Note message in dialog when chosen checkbox question type
    -Automatically added "Other" option

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormArray, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
 import { CustomizableFormGroup, questionTypes } from '../models/question';
 
@@ -25,14 +25,24 @@ export class FormService {
   constructor(
   ) { }
 
-  addQuestion(qForm : FormGroup) {
+  addQuestion(qForm: FormGroup) {
     // make it untouched.
     qForm.markAsUntouched();
     // add validation
     // if 'paragraph' type,
-    if(qForm.get('type')!.value == questionTypes.PARAGRAPH) {
+    if (qForm.get('type')!.value == questionTypes.PARAGRAPH) {
       qForm.get('answer')?.addValidators(Validators.required);
     }
+
+    // add "other" option
+    // if 'checkbox' type,
+    if (qForm.get('type')!.value == questionTypes.CHECKBOX) {
+      (qForm.get('answer') as FormArray).push(new FormGroup({
+        'option': new FormControl('Other', Validators.required),
+        'value': new FormControl(null),
+      }));
+    }
+    
     let newform = (this._form.getValue());
     (newform.get('questions') as FormArray).push(qForm);
     this.form = newform;
